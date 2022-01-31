8aae314d4043018169f5f4bb8fef342688dd4e0e
Rich Hoyt <rhoyt10@gmail.com>
2022-01-30 22:28:02 -0500

    Finish twitter clone

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+	resources :posts, only: [:index, :create] do
+		get 'like'
+		get 'repost'
+	end
+
+	root to: 'posts#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
